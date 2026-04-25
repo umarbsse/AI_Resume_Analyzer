4d75c2d8ad4d4bc0666b5bc2f34b4792be5561c9
Umer Shahzad <umarbsse@gmail.com>
2026-04-25 15:35:11 +0500

    Python initial code development and integration with Laravel controller for resume analysis.

--- app/Http/Controllers/Ai.php
+++ app/Http/Controllers/Ai.php
@@ -10,7 +10,6 @@ class Ai extends Controller
 {
 public function analyze_resume(Request $request)
 {
-    // 1. Validation
     $request->validate([
         'resume' => 'required|mimes:pdf|max:5120',
         'job_description' => 'required|string',
@@ -20,22 +19,17 @@ public function analyze_resume(Request $request)
 
         $file = $request->file('resume');
 
-        // Your custom folder path
         $destinationPath = base_path('assets/user_upload/resume');
 
-        // Create folder if not exists
         if (!file_exists($destinationPath)) {
             mkdir($destinationPath, 0777, true);
         }
 
-        // Unique filename
         $fileName = time() . '_' . $file->getClientOriginalName();
-
-        // Move file to your folder
         $file->move($destinationPath, $fileName);
 
-        // Save in DB
-        DB::table('resume_analyzer')->insert([
+        // Insert DB
+        $id = DB::table('resume_analyzer')->insertGetId([
             'file_path' => 'assets/user_upload/resume/' . $fileName,
             'original_filename' => $file->getClientOriginalName(),
             'job_description' => $request->job_description,
@@ -43,7 +37,17 @@ public function analyze_resume(Request $request)
             'updated_at' => now(),
         ]);
 
-        return back()->with('success', 'Resume uploaded successfully!');
+        // Run Python AI
+        $python = "C:\Users\John\AppData\Local\Programs\Python\Python314\python.exe";
+        $script = base_path('python/analyze_resume.py');
+        $command = "$python $script $id";
+
+
+        
+
+        exec("$python $script $id");
+
+        return back()->with('success', 'Resume uploaded and analyzed!');
     }
 
     return back()->with('error', 'No file uploaded.');
